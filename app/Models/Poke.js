b94eeed697e8a9902dc294e5275193876bc31c11
ThomF <seniorswanky@icloud.com>
2023-02-08 17:54:26 -0700

    commit

--- app/Models/Poke.js
+++ app/Models/Poke.js
@@ -15,17 +15,16 @@ export class Poke{
     get ActivePokemonTemplate() {
         return `
         <div class="col-8 m-auto p-4 spell-card rounded elevation-1 border border-dark border-2">
-            <div class="d-flex justify-content-between">
+            <div class="text-center">
             <h1>${this.name}</h1>
             </div>
-        <div class="d-flex justify-content-between">
-            <h2></h2>
-            <h2></h2>
+        <div class="text-center">
+            <img class="pokeImg" src="${this.img}">
             
         </div>
         <div class="d-flex justify-content-between">
-            <h3></h3>
-            <h3></h3>
+            <h3>Height: ${this.height}</h3>
+            <h3>Weight: ${this.weight}</h3>
         </div>
         <p class="mt-3 p-5 border border-danger border-5 rounded">
         </p>
